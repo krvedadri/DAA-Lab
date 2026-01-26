32a7001445607cdbaf075cbc74f1d3ff5443079c
KR Vedadri <krvedadri@gmail.com>
2026-01-26 19:58:16 +0530

    Implement Fibonacci series in 11249A180-1A.c
    Fix the Fibonacci function and correct includes.

---
+++ 11249A180-1A.c
@@ -0,0 +1,22 @@
+#include<sthio.h>
+int fib(int n){
+  if(n==0)
+      return 0;
+else if(n==1)
+  return 1;
+else 
+  return fib(n-1)+(n-2);
+}
+int main(){
+  int i,n;
+#include <stdio.h>
+ printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+
+    return 0;
+}
